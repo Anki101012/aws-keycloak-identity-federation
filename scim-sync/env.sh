0a85e05661d8ca1cc3803956fd96fbd72595f1ff
Ankita Sinha <118600929+Anki101012@users.noreply.github.com>
2026-03-13 14:07:00 -0500

    Create env.sh

---
+++ scim-sync/env.sh
@@ -0,0 +1,7 @@
+export KC_BASE="https://keycloak-url/keycloak"
+export KC_REALM="helixcloud"
+export KC_CLIENT_ID="scim-client-name"
+export KC_CLIENT_SECRET="xxxxx"
+
+export AWS_SCIM_BASE="https://scim.us-east-1.amazonaws.com/xxxxxxx/scim/v2"
+export AWS_SCIM_TOKEN="xxxxx"
